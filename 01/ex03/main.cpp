564d60770e4780d301e9d815d36b7e6ab03adfcf
Yeji Choi <yechoi@student.42seoul.kr>
2020-10-29 20:51:05 +0900

    cpp01 ex03 modified

--- 01/ex03/main.cpp
+++ 01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yechoi <yechoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 17:44:16 by yechoi            #+#    #+#             */
-/*   Updated: 2020/10/25 13:12:35 by yechoi           ###   ########.fr       */
+/*   Updated: 2020/10/29 20:49:27 by yechoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int     main(int argc, char **argv)
     try
     {
         N = std::stoi(argv[1]);
+		if (N <= 0)
+			return (1);
         ZombieHorde zombieHorde = ZombieHorde(N);
         zombieHorde.announce();
         return (0);
