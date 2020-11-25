cc5885dbc92148c92221ab50ecf1347011a39f1d
yechoi <yechoi@student.42seoul.kr>
2020-11-25 17:48:19 +0900

    cpp06 ex00, ex01

---
+++ 06/ex01/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yechoi <yechoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/25 17:34:05 by yechoi            #+#    #+#             */
+/*   Updated: 2020/11/25 17:34:05 by yechoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+struct Data
+{
+    std::string s1;
+    std::string s2;
+};
+
+void    *serialize(void)
+{
+    int i;
+    Data *data = new Data;
+    char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+    char alphanum[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    for (i = 0; i < 8; i++)
+        data->s1 += alpha[rand() % 25];
+    for (i = 0; i < 8 ; i++)
+        data->s2 += alphanum[rand() % 35];
+    return (data);
+}
+
+Data    *deserialize(void *raw)
+{
+    return (reinterpret_cast<Data *>(raw));
+}
+
+int main(void)
+{
+    void    *data = serialize();
+    Data    *des= deserialize(data);
+
+    std::cout << des->s1 << std::endl;
+    std::cout << des->s2 << std::endl;
+}
