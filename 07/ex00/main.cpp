511883164d6c4f100adf7c2d57d056f621bdcccd
yechoi42 <yechoi@student.42seoul.kr>
2021-01-29 17:44:00 +0900

    ADD scope resolution operator

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -20,13 +20,13 @@ int main(void)
     int b = 10;
     std::cout << "a: " << a << std::endl;
     std::cout << "b: " << b << std::endl;
-    swap(a, b);
+    ::swap(a, b);
     std::cout << "** after swap **" << std::endl;
     std::cout << "a: " << a << std::endl;
     std::cout << "b: " << b << std::endl;
-    int minimum = min(a, b);
+    int minimum = ::min(a, b);
     std::cout << "min: " << minimum << std::endl;
-    int maximum = max(a, b);
+    int maximum = ::max(a, b);
     std::cout << "max: " << maximum << std::endl;
 
     std::cout << "\033[1;31mFLOAT TEST\033[0m" << std::endl;
@@ -34,13 +34,13 @@ int main(void)
     float d = 10.3131;
     std::cout << "c: " << c << std::endl;
     std::cout << "d: " << d << std::endl;
-    swap(c, d);
+    ::swap(c, d);
     std::cout << "** after swap **" << std::endl;
     std::cout << "c: " << c << std::endl;
     std::cout << "d: " << d << std::endl;
-    float min2 = min(c, d);
+    float min2 = ::min(c, d);
     std::cout << "min: " << min2 << std::endl;
-    float max2 = max(c, d);
+    float max2 = ::max(c, d);
     std::cout << "max: " << max2 << std::endl;
 
     std::cout << "\033[1;31mCHAR TEST\033[0m" << std::endl;
@@ -49,13 +49,13 @@ int main(void)
 
     std::cout << "e: " << e << std::endl;
     std::cout << "f: " << f << std::endl;
-    swap(e, f);
+    ::swap(e, f);
     std::cout << "** after swap **" << std::endl;
     std::cout << "e: " << e << std::endl;
     std::cout << "f: " << f << std::endl;
-    int min3 = min(e, f);
+    int min3 = ::min(e, f);
     std::cout << "min: " << static_cast<char>(min3) << std::endl;
-    int max3 = max(e, f);
+    int max3 = ::max(e, f);
     std::cout << "max: " << static_cast<char>(max3) << std::endl;
 
     std::cout << "\033[1;31mSTRING TEST\033[0m" << std::endl;
@@ -64,7 +64,7 @@ int main(void)
 
     std::cout << "h: " << h << std::endl;
     std::cout << "i: " << i << std::endl;
-    swap(h, i);
+    ::swap(h, i);
     std::cout << "** after swap **" << std::endl;
     std::cout << "h: " << h << std::endl;
     std::cout << "h: " << i << std::endl;
@@ -79,7 +79,7 @@ int main(void)
 
     std::cout << "j: " << j.getN() << std::endl;
     std::cout << "k: " << k.getN() << std::endl;
-    swap(j, k);
+    ::swap(j, k);
     std::cout << "** after swap **" << std::endl;
     std::cout << "j: " << j.getN() << std::endl;
     std::cout << "k: " << k.getN() << std::endl;
